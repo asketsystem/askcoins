ad62c806ef95b57bf9db99097e37b3f30732f9fc
asketsystem <32412954+asketsystem@users.noreply.github.com>
2021-10-05 10:44:20 +0000

    Editing Homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOUtlined, MenuOutlined } from '@ant-design/icons';
 
 import icon from '../components/images/askcoins.png';
+
 const Navbar = () => {
     return (
         <div className="nav-container">
@@ -12,8 +13,12 @@ const Navbar = () => {
                 <Typography.Title level={2} className="logo">
                     <Link to="/">AskCoins</Link>
                 </Typography.Title>
-                <Button className="menu-control-container"></Button>
             </div>
+            <Menu theme="dark">
+                <Menu.Item icon={<HomeOutlined />}>
+                    <Link to="/">Home</Link>
+                </Menu.Item>
+            </Menu>
             
         </div>
     )
