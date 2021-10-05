3d46bd6b84ca9edf6cbeb5318019772f5f7a53d0
asketsystem <32412954+asketsystem@users.noreply.github.com>
2021-10-05 19:24:05 +0000

    Setting up footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
-import { Navbar,  } from './components';
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components';
+
+import 'antd/dist/antd.css'
 import './App.css';
 
 const App = () => {
@@ -11,11 +13,42 @@ const App = () => {
             <div className="navbar"> 
                 <Navbar />
             </div>
-            <div className="main"></div>
-            <div className="footer"></div>
-            
+            <div className="main">
+                <Layout>
+                    <div className="routes">
+                        <Switch>
+                            <Route exact path="/">
+                                <Homepage />
+                            </Route>
+                            <Route exact path="/exchanges">
+                                <Exchanges />
+                            </Route>
+                            <Route exact path="/cryptocurrencies">
+                                <Cryptocurrencies />
+                            </Route>
+                            <Route exact path="/crypto/:coinId">
+                                <CryptoDetails />
+                            </Route>
+                            <Route exact path="/news">
+                                <News />
+                            </Route>
+                        </Switch>
+                    </div>
+                </Layout>
+                <div className="footer">
+                    <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+                        Askcoins <br />
+                        All rights reserved
+                    </Typography.Title>
+                    <Space>
+                        <Link to="/">Home</Link>
+                        <Link to="/exchanges">Exchanges</Link>
+                        <Link to="news">News</Link>
+                    </Space>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default App
