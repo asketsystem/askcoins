edcf2e647e31c230207d3a8be0677670e3a5da15
asketsystem <32412954+asketsystem@users.noreply.github.com>
2021-10-05 16:47:17 +0000

    Updating Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
-import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOUtlined, MenuOutlined } from '@ant-design/icons';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
 import icon from '../components/images/askcoins.png';
 
@@ -16,6 +16,18 @@ const Navbar = () => {
             </div>
             <Menu theme="dark">
                 <Menu.Item icon={<HomeOutlined />}>
+                    <Link to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined />}>
+                    <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined />}>
+                    <Link to="/exchanges">Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BulbOutlined />}>
+                    <Link to="/">News</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MenuOutlined />}>
                     <Link to="/">Home</Link>
                 </Menu.Item>
             </Menu>
